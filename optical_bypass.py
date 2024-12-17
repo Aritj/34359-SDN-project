c196c62eda80cdb608b6904af5f090912c8868a3
AriTJ <Ari.Joh20@student.setur.fo>
2024-12-17 22:33:10 +0000

    Added assignment pdf and refactored network topology code

--- optical_bypass.py
+++ optical_bypass.py
@@ -1,66 +1,54 @@
 #!/usr/bin/python
-
 from mininet.net import Mininet
 from mininet.node import RemoteController, OVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 from mininet.link import TCLink
 
-class OpticalBypassTopo:
-    def __init__(self):
-        self.net = Mininet(controller=RemoteController, switch=OVSSwitch, link=TCLink)
-
-        # Add ONOS controller
-        self.controller = self.net.addController(
-            'c0',
-            controller=RemoteController,
-            ip='127.0.0.1',
-            port=6633,
-        )
-
-        # Create leaf switches (TOR)
-        leafs = [self.net.addSwitch('s{}'.format(i), protocols='OpenFlow13') for i in range(1,5)]
-
-        # Create spine switches
-        e_spine = self.net.addSwitch('s5', protocols='OpenFlow13')
-        o_spine = self.net.addSwitch('s6', protocols='OpenFlow13')
-
-
-        # Electrical fabric connections are high latency, low bandwidth.
-        e_bw = 10  # electrical bandwidth
-        e_delay = "100ms"  # electrical delay
-
-        # Optical bypass fabric connections are low latency, high bandwidth.
-        o_bw = 1000  # optical bandwidth
-        o_delay = "0.01ms"  # optical delay
-
-        # Link each leaf switch to both spine switches
-        for i, leaf in enumerate(leafs):
-            self.net.addLink(leaf, e_spine, bw=e_bw, delay=e_delay)
-            self.net.addLink(leaf, o_spine, bw=o_bw, delay=o_delay)
-
-            # Add hosts
-            for j in range(1,5):
-                host = self.net.addHost('h{}{}'.format(i, j), ip='10.0.{}.{}'.format(i, j))
-                self.net.addLink(host, leaf)
-
-
-    def start(self):
-        self.net.build()
-        self.controller.start()
-        for switch in self.net.switches:
-            switch.start([self.controller])
-        self.net.start()
-        CLI(self.net)
-
-    def stop(self):
-        self.net.stop()
-
-def main():
+def main(leaf_switch_count = 4, hosts_per_leaf_count = 4):
+    # Set logging level
     setLogLevel('info')
-    topo = OpticalBypassTopo()
-    topo.start()
+
+    # Create the network
+    net = Mininet(controller=RemoteController, switch=OVSSwitch, link=TCLink)
+
+    # Add ONOS controller
+    controller = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
+
+    # Define leaf-spine fabric connection parameters
+    electrical_params = {'bw': 10, 'delay': "100ms"} # High latency, low bandwidth
+    optical_params = {'bw': 1000, 'delay': "0.01ms"} # Low latency, high bandwidth
+    
+    # Create spine switches
+    e_spine = net.addSwitch('s{}'.format(leaf_switch_count+1), protocols='OpenFlow13')
+    o_spine = net.addSwitch('s{}'.format(leaf_switch_count+2), protocols='OpenFlow13')
+ 
+    # Create and link each leaf switch to both spine switches
+    for leaf_index in range(1, leaf_switch_count):
+        leaf = net.addSwitch('s{}'.format(leaf_index), protocols='OpenFlow13')
+        net.addLink(leaf, e_spine, **electrical_params)
+        net.addLink(leaf, o_spine, **optical_params)
+
+        # Add hosts
+        for host_index in range(1, hosts_per_leaf_count + 1):
+            host_name = 'h{}{}'.format(leaf_index, host_index)
+            host_ip = '10.0.{}.{}'.format(leaf_index, host_index)
+            host = net.addHost(host_name, ip=host_ip)
+            
+            net.addLink(host, leaf)
+
+    # Start the network
+    net.build()
+    controller.start()
+    for switch in net.switches:
+        switch.start([controller])
+    net.start()
+
+    # Open the CLI
+    CLI(net)
+
+    # Stop the network
+    net.stop()
 
 if __name__ == '__main__':
     main()
-
